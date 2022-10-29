d16adfc207c4bf312d35819b17c6bebeef5053a9
tarikwaleed <takizee53@gmail.com>
2022-10-29 14:11:16 +0200

    Done with assignment1

---
+++ iti-c-labs/assignment1/hello.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+void printHelloWorld()
+{
+    printf("Hello, World!");
+}
+void printAsciiChar(char c)
+{
+    printf("%d", c);
+}
+void printFloat()
+{
+    printf("Please Enter a float number: ");
+    float number;
+    scanf("%f", &number);
+    printf("The number you've entered is: %f", number);
+}
+int adder(int x, int y)
+{
+    return x + y;
+}
+int multiplier(int x, int y)
+{
+    return x * y;
+}
+int divider(int x, int y)
+{
+    return x / y;
+}
+int subtractor(int x, int y)
+{
+    return x - y;
+}
+void hexaPrinter(int number)
+{
+    printf("%X", number);
+}
+void qoutientAndReminder(int x, int y)
+{
+    printf("Quotient is:%d\n", x / y);
+    printf("Reminder is %d\n", x % y);
+}
+int main(int argc, char const *argv[])
+{
+    // printHelloWorld();
+    // printFloat();
+    // printAsciiChar('b');
+    // int result = multiplier(5, 6);
+    // printf("%d", result);
+
+    // hexaPrinter(190);
+    qoutientAndReminder(122, 5);
+
+    return 0;
+}
